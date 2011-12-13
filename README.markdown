d4d0e74910dd0f985ea267678c67d2721909b0ee
Jeff Bain <Jeff@JeffBain.ca>
2011-12-13 18:11:18 -0500

    Modify Collection._add to insert properly
        add now uses the currently set comparator settable via sort() to insert in order
        update README to reflect change

--- README.markdown
+++ README.markdown
@@ -7,4 +7,4 @@ Just a little handy extension to the Backbone Collection class. Intended to add 
 ## Collection.sort()
 Collection.sort now accepts 2 arguments:
 * silent: behaves exactly the same as in vanilla Backbone. suppresses "reset" event triggering on sort
-* comparator: overrides the default comparator for Backbone. For now, this just resorts the array.
+* comparator: overrides the default comparator for Backbone. This becomes "active comparator" and inserts will keep the collection ordered via this comparator until the list is sorted by a new comparator
--- resortable.js
+++ resortable.js
@@ -2,7 +2,11 @@
 // to sorting by multiple criteria
 
 var ResortableCollection = Backbone.Collection.extend({
-  sort : function(options) {
+  // sort: This takes the Backbone sort function and adds some logic to check
+  // for a comparator object to override it. It also stores the current
+  // Comparator as _comparator in contrast to comparator which is treated
+  // as the default
+  sort: function(options) {
     options || (options = {})
     if (options.comparator) {
       // Set current comparitor so add knows how to do
@@ -13,5 +17,24 @@ var ResortableCollection = Backbone.Collection.extend({
     } else throw new Error('Cannot sort a set without a comparator')
     this.models = this.sortBy(this._comparator)
     if (!options.silent) this.trigger('reset', this, options)
+  },
+
+  // This function mirrors the Backbone _add function but uses _comparator instead of comparator as its comparator to pass to sortedIndex
+  _add: function(model, options) {
+    options || (options = {})
+    model = this._prepareModel(model, options)
+    if (!model) return false
+    var already = this.getByCid(model)
+    if (already) throw new Error(["Can't add the same model to a set twice", already.id])
+    this._byId[model.id] = model
+    this._byCid[model.cid] = model
+    var index = options.at != null? options.at :
+                this._comparator ? this.sortedIndex(model, this.comparator) :
+                this.length
+    this.model.splice(index, 0, model)
+    model.bind('all', this._onModelEvent)
+    this.length++
+    if (!options.silent) model.trigger('add', model, this, options)
+    return model
   }
 })
